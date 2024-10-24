7be425b902445779781c56ac0025c8270632ee9f
Guo-Alyssa-Lyn <guoalyssalyn201510@gmail.com>
2024-10-24 21:39:53 +0800

    Added my Personal Portfolio project

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+
+
+/*
+* - To Reset The Contact Form Texts
+*/ 
+
+// Function to reset the form after submission
+function resetFormAfterSubmit() {
+
+  document.querySelector(".contact-form").reset();
+}
+
+// Check if the page is loaded due to a form submission (navigated back)
+window.addEventListener('pageshow', function(event) {
+  if (event.persisted || (window.performance && window.performance.navigation.type === 2)) {
+      document.querySelector(".contact-form").reset();
+  }
+});
+
+
